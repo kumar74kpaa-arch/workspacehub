4a1df27a4d8a1e060b06315fba48958f7bdc272f
Kumar Technologies <kumartechnologies2025@gmail.com>
2026-02-08 16:37:18 +0000

    Check Your Firebase Config (VERY IMPORTANT)
    Open your code:
    
    firebase.t

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,7 +3,7 @@
 // Make sure to fill in your Firebase project configuration
 export const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  authDomain: `${process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID}.firebaseapp.com`,
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
